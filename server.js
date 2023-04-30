7be341fe629f5e700cff4c046aef7351c17e953b
farbodfld <farbodfooladi@gmail.com>
2023-04-30 13:41:28 +0330

    initial project

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.send("Hello API")
+})
+
+const PORT = process.env.port || 8080 
+
+app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`)
+})
