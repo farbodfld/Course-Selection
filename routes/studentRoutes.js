98585a5082c4188cf0d27dfbd4309a54b767a16b
farbodfld <farbodfooladi@gmail.com>
2023-05-12 00:55:40 +0330

    swagger, student APIs

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -7,9 +7,27 @@ const router = require("express").Router()
  *     Student:
  *       type: object
  *       properties:
- *         id:
+ *         firstname:
  *           type: string
- *           description: The auto-generated id of the Student
+ *           description: The firstname of the Professor
+ *         surname:
+ *           type: string
+ *           description: The surname of your Professor
+ *         userNumber:
+ *           type: number
+ *           description: The userNumber of your Professor
+ *         password:
+ *           type: string
+ *           description: The password of your Professor
+ *         email:
+ *            type: string
+ *            description: The email of your Professor
+ *         mobilePhone:
+ *           type: string
+ *           description: The mobilePhone of your Professor
+ *         role:
+ *           type: string
+ *           description: The role of your Professor
  *         educationalLevel:
  *           type: string
  *           description: The educationalLevel of your Student
@@ -25,6 +43,172 @@ const router = require("express").Router()
  *         faculty:
  *           type: string
  *           description: The faculty of your Student
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * components:
+ *   responses:
+ *     UnauthorizedError:
+ *       description: Access token is missing or invalid
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Students
+ *   description: The Student managing API
+ * /api/admin/student:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Lists of all the Students
+ *     tags: [Students]
+ *     responses:
+ *       200:
+ *         description: The list of the Students
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Student'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ * 
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Create a new Student
+ *     tags: [Students]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Student'
+ *     responses:
+ *       200:
+ *         description: The created Student.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Student'
+ *       500:
+ *         description: Some server error
+ * 
+ * /api/admin/student/{id}:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get the Student by id
+ *     tags: [Students]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Student id
+ *     responses:
+ *       200:
+ *         description: The Student response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Student'
+ *       404:
+ *         description: The Student was not found
+ * 
+ *   put:
+ *    security:
+ *      - bearerAuth: []
+ *    summary: Update the Student by the id
+ *    tags: [Students]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The Student id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Student'
+ *    responses:
+ *      200:
+ *        description: The Student was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Student'
+ *      404:
+ *        description: The Student was not found
+ *      500:
+ *        description: Some error happened
+ * 
+ *   delete:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Remove the Student by id
+ *     tags: [Students]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Student id
+ *
+ *     responses:
+ *       200:
+ *         description: The Student was deleted
+ *       404:
+ *         description: The Student was not found
+ * 
+ * /api/student/{id}:
+ *   put:
+ *    security:
+ *      - bearerAuth: []
+ *    summary: Student update his profile by the id
+ *    tags: [Students]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The Student id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Student'
+ *    responses:
+ *      200:
+ *        description: The Student was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Student'
+ *      404:
+ *        description: The Student was not found
+ *      500:
+ *        description: Some error happened
  */
 
 const student_controller = require("../controller/studentController")
