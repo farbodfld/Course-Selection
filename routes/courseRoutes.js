bdc700461bdd1abc51509e2569c9cb321254719d
farbodfld <farbodfooladi@gmail.com>
2023-05-12 01:15:12 +0330

    swagger, course APIs

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -42,6 +42,131 @@ const router = require("express").Router()
  *           description: The academicSemester of your Course
  */
 
+/**
+ * @swagger
+ * tags:
+ *   name: Courses
+ *   description: The Course managing API
+ * /api/course:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Lists of all the Courses
+ *     tags: [Courses]
+ *     responses:
+ *       200:
+ *         description: The list of the Courses
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Course'
+ *       401:
+ *         description: Access token is missing or invalid
+ * 
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Create a new Course
+ *     tags: [Courses]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Course'
+ *     responses:
+ *       200:
+ *         description: The created Course.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Course'
+ *       500:
+ *         description: Some server error
+ *       401:
+ *         description: Access token is missing or invalid
+ * 
+ * /api/course/{id}:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get the Course by id
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Course id
+ *     responses:
+ *       200:
+ *         description: The Course response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Course'
+ *       404:
+ *         description: The Course was not found
+ *       401:
+ *         description: Access token is missing or invalid
+ * 
+ *   put:
+ *    security:
+ *      - bearerAuth: []
+ *    summary: Update the Course by the id
+ *    tags: [Courses]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The Course id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Course'
+ *    responses:
+ *      200:
+ *        description: The Course was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Course'
+ *      404:
+ *        description: The Course was not found
+ *      500:
+ *        description: Some error happened
+ *      401:
+ *        description: Access token is missing or invalid
+ * 
+ *   delete:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Remove the Course by id
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Course id
+ *
+ *     responses:
+ *       200:
+ *         description: The Course was deleted
+ *       404:
+ *         description: The Course was not found
+ *       401:
+ *         description: Access token is missing or invalid
+ */
+
 let course_controller = require("../controller/courseController")
 
 let tokenValidation = require("../middleware/tokenValidation")
