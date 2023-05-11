f4c0a40b24bfee1fd5d82446ca80a8fa8b7f4295
farbodfld <farbodfooladi@gmail.com>
2023-05-12 00:22:56 +0330

    swagger, professor APIs

--- routes/professorRoutes.js
+++ routes/professorRoutes.js
@@ -7,9 +7,27 @@ const router = require("express").Router()
  *     Professor:
  *       type: object
  *       properties:
- *         id:
+ *         firstname:
  *           type: string
- *           description: The auto-generated id of the Professor
+ *           description: The firstname of the Professor
+ *         surname:
+ *           type: string
+ *           description: The surname of your Professor
+ *         userNumber:
+ *           type: number
+ *           description: The userNumber of your Professor
+ *         password:
+ *           type: string
+ *           description: The password of your Professor
+ *         email:
+ *            type: string
+ *            description: The email of your Professor
+ *         mobilePhone:
+ *           type: string
+ *           description: The mobilePhone of your Professor
+ *         role:
+ *           type: string
+ *           description: The role of your Professor
  *         faculty:
  *           type: string
  *           description: The faculty of your Professor
@@ -17,8 +35,143 @@ const router = require("express").Router()
  *           type: string
  *           description: The field of your Professor
  *         order:
- *           type: number
+ *           type: string
  *           description: The order of your Professor
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * components:
+ *   responses:
+ *     UnauthorizedError:
+ *       description: Access token is missing or invalid
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Professors
+ *   description: The Professor managing API
+ * /api/admin/professor:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Lists of all the professors
+ *     tags: [Professors]
+ *     responses:
+ *       200:
+ *         description: The list of the Professors
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Professor'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ * 
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Create a new Professor
+ *     tags: [Professors]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Professor'
+ *     responses:
+ *       200:
+ *         description: The created Professor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Professor'
+ *       500:
+ *         description: Some server error
+ * 
+ * /api/admin/professor/{id}:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get the Professor by id
+ *     tags: [Professors]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Professor id
+ *     responses:
+ *       200:
+ *         description: The Professor response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Professor'
+ *       404:
+ *         description: The Professor was not found
+ * 
+ *   put:
+ *    security:
+ *      - bearerAuth: []
+ *    summary: Update the Professor by the id
+ *    tags: [Professors]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The Professor id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Professor'
+ *    responses:
+ *      200:
+ *        description: The Professor was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Professor'
+ *      404:
+ *        description: The Professor was not found
+ *      500:
+ *        description: Some error happened
+ * 
+ *   delete:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Remove the Professor by id
+ *     tags: [Professors]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Professor id
+ *
+ *     responses:
+ *       200:
+ *         description: The Professor was deleted
+ *       404:
+ *         description: The Professor was not found
  */
 
 const professor_controller = require("../controller/professorController")
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,34 +7,13 @@ const router = express.Router()
  *   schemas:
  *     User:
  *       type: object
- *       required:
- *         - role
  *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the User
- *         firstname:
- *           type: string
- *           description: The firstname of your user
- *         surname:
- *           type: string
- *           description: The surname of your user
- *         userNumber:
- *           type: number
- *           description: The userNumber of your user
- *         password:
- *           type: string
- *           description: The password of your user
  *         email:
  *           type: string
  *           description: The email of your user
- *         mobilePhone:
+ *         password:
  *           type: string
- *           description: The mobilePhone of your user
- *         role:
- *           type: string
- *           description: The role of your user
- *     
+ *           description: The password of your user
  */
 
 /**
