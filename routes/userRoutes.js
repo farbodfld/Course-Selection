813236158b6cfb9b31b0dae52d7d137831c0dff2
farbodfld <farbodfooladi@gmail.com>
2023-05-11 13:47:07 +0330

    swagger, user login (admin/manager, create user API)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -37,6 +37,51 @@ const router = express.Router()
  *     
  */
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: The Users managing API
+ * /api/createAdmin:
+ *   post:
+ *     summary: Create a new User
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The created User.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ * /api/login:
+ *   post:
+ *     summary: Login User
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User logedin.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ */
+
 const userObject = require("../controller/userController")
 
 router.route("/createAdmin").post(userObject.creatAdmin)
