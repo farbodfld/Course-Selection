813fe33954688787c51642e5d4e2905b954766eb
Parham Barati <bara0131@algonquinlive.com>
2023-07-01 14:57:42 -0400

    add api

--- Front/course-selection/src/components/TA/Semesters/Semesters.js
+++ Front/course-selection/src/components/TA/Semesters/Semesters.js
@@ -1,15 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import './Semesters.css';
-import allSemesters from "../../../mockdata";
+//import allSemesters from "../../../mockdata";
 import { Button } from '@mui/material';
 import { useSelector } from "react-redux";
+import {Authentication , NavigateToRole} from '../../../Authentication/Authentication'
 
 const Semesters = () => {
-  const [semesters, setSemesters] = useState(allSemesters.slice(0, 10));
+  const [semesters, setSemesters] = useState([]);
+  const [allSemesters, setallSemesters] = useState([])
   const [showAll, setShowAll] = useState(false);
   const { mode } = useSelector((state) => state.darkMode);
+
+  
+  useEffect(() => {
+
+
+    const fetchTerms = async () => {
+      try {
+        const accessToken = localStorage.getItem("accessToken");
+
+        const response = await fetch("http://localhost:9090/api/terms", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const data = await response.json();
+        console.log(data);
+        setallSemesters(data)
+        setSemesters(data.slice(0, 6))
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchTerms();
+
+   
+  }, []);
   
   const handleShowMore = () => {
     setShowAll(true);
@@ -28,7 +59,7 @@ const Semesters = () => {
       <div className='semesters-container'>
         <ul className='list'>
           {semesters.map((semester) => (
-            <Link key={semester.id} to={`/teacher-assistant/${semester.id}`}>
+            <Link key={semester.id} to={`/teacher-assistant/1`}>
               <Card className='card'>
                 <li>
                   <p>{semester.name}</p>
@@ -37,7 +68,7 @@ const Semesters = () => {
             </Link>
           ))}
         </ul>
-        {semesters.length > 6 && (
+        {allSemesters.length > 6 && (
           <div className='btn-container'>
             {!showAll ? (
               <Button className='showMore btn' onClick={handleShowMore}>
