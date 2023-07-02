b8ddd53aaac5694848635a66de7103c1d167e56d
farbodfld <farbodfooladi@gmail.com>
2023-07-02 23:41:12 +0330

    educatinal manager swagger added

--- Backend/routes/educationalManagerRoutes.js
+++ Backend/routes/educationalManagerRoutes.js
@@ -1,3 +1,385 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Semester:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         courses:
+ *           type: string
+ *           description: List of courses.
+ *         users:
+ *           type: number
+ *           description: List of student number.
+ *         preregistration_courses:
+ *           type: string
+ *           description: List of pre-regist courses.
+ *         registration_courses:
+ *            type: string
+ *            description: List of pre-regist courses.
+ *         
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Semesters
+ * /api/terms:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Semesters]
+ *     responses:
+ *       200:
+ *         description: The list of the Terms
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Semester'
+ *       401:
+ *         description: Access token is missing or invalid
+ * /api/term:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Semesters]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Semesters'
+ *     responses:
+ *       200:
+ *         description: The created Manager.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Semester'
+ *       500:
+ *         description: Some server error
+ *       401:
+ *         description: Access token is missing or invalid
+ * 
+ * /api/term/{id}:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Get the Manager by id
+ *     tags: [Semesters]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Semester id
+ *     responses:
+ *       200:
+ *         description: The Semester response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Semester'
+ *       404:
+ *         description: The Semester was not found
+ *       401:
+ *         description: Access token is missing or invalid
+ * 
+ *   put:
+ *    security:
+ *      - bearerAuth: []
+ *    summary: Update the Semester by the id
+ *    tags: [Semesters]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The Semester id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Semester'
+ *    responses:
+ *      200:
+ *        description: The Semester was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Semester'
+ *      404:
+ *        description: The Semester was not found
+ *      500:
+ *        description: Some error happened
+ *      401:
+ *        description: Access token is missing or invalid
+ * 
+ *   delete:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Remove the Semester by id
+ *     tags: [Semesters]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Semester id
+ *
+ *     responses:
+ *       200:
+ *         description: The Semester was deleted
+ *       404:
+ *         description: The Semester was not found
+ *       401:
+ *         description: Access token is missing or invalid
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Pre-Registeration Courses:
+ *       type: object
+ *       properties:
+ *         preregistration_courses:
+ *           type: string
+ *         course:
+ *           type: string
+ *           description: List of courses.
+ *         
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Pre-Registeration Courses
+ * /api/term/{id}/preregistration_courses:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Pre-Registeration Courses]
+ *     responses:
+ *       200:
+ *         description: The list of the Terms
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Pre-Registeration Courses'
+ *       401:
+ *         description: Access token is missing or invalid
+ * /api/term/{id}/preregistration:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Pre-Registeration Courses]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Pre-Registeration Courses'
+ *     responses:
+ *       200:
+ *         description: The created Manager.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Pre-Registeration Courses'
+ *       500:
+ *         description: Some server error
+ *       401:
+ *         description: Access token is missing or invalid
+ * 
+ *   delete:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Remove the Semester by id
+ *     tags: [Pre-Registeration Courses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Semester id
+ *
+ *     responses:
+ *       200:
+ *         description: The Semester was deleted
+ *       404:
+ *         description: The Semester was not found
+ *       401:
+ *         description: Access token is missing or invalid
+ *
+ * /api/term/{id}/preregistrations:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Pre-Registeration Courses]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Pre-Registeration Courses'
+ *     responses:
+ *       200:
+ *         description: The created Manager.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Pre-Registeration Courses'
+ *       500:
+ *         description: Some server error
+ *       401:
+ *         description: Access token is missing or invalid
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Registeration Courses:
+ *       type: object
+ *       properties:
+ *         registration_courses:
+ *           type: string
+ *         course:
+ *           type: string
+ *           description: Name of course.
+ *         
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Registeration Courses
+ * /api/term/{id}/registration_courses:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Registeration Courses]
+ *     responses:
+ *       200:
+ *         description: The list of the Terms
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Registeration Courses'
+ *       401:
+ *         description: Access token is missing or invalid
+ * /api/term/{id}/registration:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Registeration Courses]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Registeration Courses'
+ *     responses:
+ *       200:
+ *         description: The created Manager.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Registeration Courses'
+ *       500:
+ *         description: Some server error
+ *       401:
+ *         description: Access token is missing or invalid
+ * 
+ *   delete:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Remove the Semester by id
+ *     tags: [Registeration Courses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Semester id
+ *
+ *     responses:
+ *       200:
+ *         description: The Semester was deleted
+ *       404:
+ *         description: The Semester was not found
+ *       401:
+ *         description: Access token is missing or invalid
+ *
+ * /api/term/{id}/registrations:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Registeration Courses]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Registeration Courses'
+ *     responses:
+ *       200:
+ *         description: The created Manager.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Registeration Courses'
+ *       500:
+ *         description: Some server error
+ *       401:
+ *         description: Access token is missing or invalid
+ */
+
 const router = require("express").Router()
 const educationalManagerController = require('../controller/educationalManagerController');
 const { getPreregReqs, createPreregReq, addCourseToPreregReq } = require("../controller/courseRequestController");
